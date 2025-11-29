92f5d59a6966d117857b3a7abf9724efe3cea809
7Omarsaeed7 <os391480@gmail.com>
2025-11-29 14:32:28 +0200

    Delete frontend directory

--- frontend/src/hooks/useCart.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { CartContext } from "../Context/CartContext/CartContext";
-function useCart() {
-  const context = useContext(CartContext);
-  return context;
-}
-export { useCart };
